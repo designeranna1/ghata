b03b0ae710857e5907db895a666b7188100a4993
Vasanth Developer <vasanth@vasanthdeveloper.com>
2020-04-28 12:41:44 +0530

    🎉 I want this to be perfect! just perfect!

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+ 
+//   ___    __________   |  Vasanth Developer (Vasanth Srivatsa)
+//   __ |  / /___  __ \  |  ------------------------------------------------
+//   __ | / / __  / / /  |  https://github.com/vasanthdeveloper/hrasvaka.git
+//   __ |/ /  _  /_/ /   |
+//   _____/   /_____/    |  Prettier run control
+//                       |
+
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    singleQuote: true,
+    useTabs: false,
+    quoteProps: 'as-needed',
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    parser: 'typescript',
+    endOfLine: 'lf',
+}
---
+++ src/exe/index.ts
@@ -0,0 +1,8 @@
+//   ___    __________   |  Vasanth Developer (Vasanth Srivatsa)
+//   __ |  / /___  __ \  |  ------------------------------------------------
+//   __ | / / __  / / /  |  https://github.com/vasanthdeveloper/ghata.git
+//   __ |/ /  _  /_/ /   |
+//   _____/   /_____/    |  Executable entryfile for ghata project
+//                       |
+
+console.log('Hello World!')
